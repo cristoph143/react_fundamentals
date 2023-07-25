3bb8d6e8f180a425de2c21397b90735b6b1fa01c
Cristopher Bacus Bohol <cris.bacus6@gmail.com>
2023-07-25 23:03:43 +0800

    inline style

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,11 +1,28 @@
 const Navbar = () => {
   return (
     <>
-      <nav className="navbar">
-        <ul><a href="#">hello</a></ul>
-        <ul><a href="#">hello</a></ul>
-        <ul><a href="#">hello</a></ul>
-      </nav>
+      <div className="navbar" style={{
+        padding: 0,
+        margin: 0,
+        border: 0,
+        display: "flex"
+      }}>
+        <h1 className="navbar-title">The Dojo Blog</h1>
+        <nav className="navbar" style={{
+            display: "flex",
+            justifyContent: "space-around",
+            alignItems: "center",
+        }}>
+          <div className="links" style={{
+            display: "flex",
+            justifyContent: "space-around",
+            alignItems: "center",
+          }}>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+          </div>
+        </nav>
+      </div>
     </>
   );
 };
