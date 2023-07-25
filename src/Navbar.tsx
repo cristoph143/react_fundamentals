7e993887a619df26218da24649bba4b299f58716
Cristopher Bacus Bohol <cris.bacus6@gmail.com>
2023-07-25 23:06:31 +0800

    modify css

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,28 +1,22 @@
 const Navbar = () => {
   return (
     <>
-      <div className="navbar" style={{
-        padding: 0,
-        margin: 0,
-        border: 0,
-        display: "flex"
-      }}>
-        <h1 className="navbar-title">The Dojo Blog</h1>
-        <nav className="navbar" style={{
-            display: "flex",
-            justifyContent: "space-around",
-            alignItems: "center",
-        }}>
-          <div className="links" style={{
-            display: "flex",
-            justifyContent: "space-around",
-            alignItems: "center",
-          }}>
-            <a href="/">Home</a>
-            <a href="/about">About</a>
-          </div>
-        </nav>
-      </div>
+      <nav className="navbar">
+        <h1>The Dojo Blog</h1>
+        <div className="links">
+          <a href="/">Home</a>
+          <a
+            href="/create"
+            style={{
+              color: "white",
+              backgroundColor: "#f1356d",
+              borderRadius: "8px",
+            }}
+          >
+            New Blog
+          </a>
+        </div>
+      </nav>
     </>
   );
 };
