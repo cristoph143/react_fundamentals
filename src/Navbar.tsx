dc628ca3babfca99948fcfa4bc23fd87e46da06f
Cristopher Bacus Bohol <cris.bacus6@gmail.com>
2023-07-25 22:49:56 +0800

    multiple component

---
+++ src/Navbar.tsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <>
+      <nav className="navbar">
+        <ul><a href="#">hello</a></ul>
+        <ul><a href="#">hello</a></ul>
+        <ul><a href="#">hello</a></ul>
+      </nav>
+    </>
+  );
+};
+export default Navbar;
