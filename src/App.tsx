5c8ded82748c9d86ab86362d53e75d4f1371a073
Cristopher Bacus Bohol <cris.bacus6@gmail.com>
2023-07-26 21:23:44 +0800

    react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import "./App.css";
-import Navbar from "./Navbar";
+// import List from "./List";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Home";
+import Navbar from "./Navbar";
 import List from "./List";
+
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Home />
-      <List />
-    </>
+      <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/list" element={<List />} />
+      </Routes>
+    </Router>
   );
 }
 
