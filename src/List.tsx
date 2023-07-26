810d577facebdb56dacff7775d5cd40e5754bd7a
Cristopher Bacus Bohol <cris.bacus6@gmail.com>
2023-07-26 19:31:23 +0800

    fetching data with useEffect

--- src/List.tsx
+++ src/List.tsx
@@ -1,40 +1,48 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
-
+// npx json-server --watch data/db.json --port 8000 -> run json server
 const List = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-    {
-      title: "Web dev top tips",
-      body: "lorem ipsum...",
-      author: "mario",
-      id: 3,
-    },
-  ]);
+  const [blogs, setBlogs] = useState(null);
   const handleDeleteBlog = (id) => {
     // delete id from blogs list
     setBlogs(blogs.filter((blog) => blog.id !== id));
   };
 
   const title = "Welcome! All Blogs Ahead!";
-  const title1 = "Mario's Blog!";
-  console.log(blogs);
-  const mario = blogs.filter((blog) => blog.author === "mario");
-  console.log(mario);
 
   const [names, setName] = useState("mario");
+  //   useEffect(() => {
+  //     console.log("use effect ran");
+  //     console.log(names);
+  //   }, [names]);
   useEffect(() => {
-    console.log("use effect ran");
-    console.log(names);
-  }, [names]);
-
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setBlogs(data);
+      });
+  }, []);
+//   const title1 = "Mario's Blog!";
+//   console.log(blogs);
+//   const mario = blogs.filter((blog) => blog.author === "mario");
+//   console.log(mario);
   return (
     <>
-      <BlogList blogs={blogs} title={title} handleDelete={handleDeleteBlog} />
-      <BlogList blogs={mario} title={title1} handleDelete={handleDeleteBlog} />
+      {blogs && (
+        <BlogList blogs={blogs} title={title} handleDelete={handleDeleteBlog} />
+      )}
+      {/* {blogs && (
+        <BlogList
+          blogs={mario}
+          title={title1}
+          handleDelete={handleDeleteBlog}
+        />
+      )} */}
       <button onClick={() => setName("luigi")}>change name</button>
-        <p>{names}</p>
+      <p>{names}</p>
     </>
   );
 };
