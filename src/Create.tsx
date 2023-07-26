e7ee919c37453c38ee753e41a8fd5c506051236d
Cristopher Bacus Bohol <cris.bacus6@gmail.com>
2023-07-26 23:08:53 +0800

    post request

--- src/Create.tsx
+++ src/Create.tsx
@@ -4,11 +4,34 @@ const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
+  const [isPending, setIsPending] = useState(false)
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    // send blog to API
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    })
+      .then(() => {
+        setIsPending(true)
+        console.log("new blog added");
+        setTitle("");
+        setBody("");
+        setAuthor("");
+      })
+      .catch((err) => {
+        setIsPending(false)
+        console.log(err);
+      });
+  };
   return (
     <>
       <div className="create">
         <h2 className="title">Add a New Blog</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label>Blog Title:</label>
           <input
             type="text"
@@ -31,7 +54,8 @@ const Create = () => {
             <option value="mario">mario</option>
             <option value="yoshi">yoshi</option>
           </select>
-          <button>Add Blog</button>
+          {!isPending && <button>Add Blog</button>}
+          {isPending && <button disabled>Add Blog</button>}
           <p>{title}</p>
           <p>{body}</p>
           <p>{author}</p>
