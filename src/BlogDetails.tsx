42ff18cfc2f03bf6f8a88e0b2cbe676ae16c88c8
Cristopher Bacus Bohol <cris.bacus6@gmail.com>
2023-07-26 23:25:43 +0800

    delete blogs

--- src/BlogDetails.tsx
+++ src/BlogDetails.tsx
@@ -1,13 +1,25 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
+import { useNavigate } from "react-router-dom";
 
 const BlogDetails = () => {
   const { id } = useParams();
-  const { data: blogs, isPending, error } = useFetch("http://localhost:8000/blogs");
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
+  const navigate = useNavigate();
 
   // Find the blog with the matching ID
   const blog = blogs?.find((blog) => blog.id === parseInt(id));
-
+  const handleClick = () => {
+    fetch(`http://localhost:8000/blogs/${blog.id}`, {
+      method: "DELETE",
+    }).then(() => {
+      navigate("/");
+    });
+  };
   return (
     <div className="blog-details">
       {isPending && <div className="isLoading">Loading</div>}
@@ -15,6 +27,7 @@ const BlogDetails = () => {
       {blog && (
         <article key={blog.id}>
           <h2 className="title">{blog.title}</h2>
+          <button onClick={handleClick}>Delete</button>
           <p>Written by: {blog.author}</p>
           <div className="body">{blog.body}</div>
         </article>
@@ -22,6 +35,5 @@ const BlogDetails = () => {
     </div>
   );
 };
-
 
 export default BlogDetails;
