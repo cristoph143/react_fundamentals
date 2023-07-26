c2a599331fa13bae95552d7a82cb52addd920e8c
Cristopher Bacus Bohol <cris.bacus6@gmail.com>
2023-07-26 18:54:03 +0800

    reusable component

--- src/List.tsx
+++ src/List.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import BlogList from "./BlogList";
 
 const List = () => {
-  const [blog, setBlogs] = useState([
+  const [blogs, setBlogs] = useState([
     { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
     { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
     {
@@ -14,9 +14,14 @@ const List = () => {
   ]);
 
   const title = 'Welcome! All Blogs Ahead!'
+  const title1 = 'Mario\'s Blog!'
+  console.log(blogs);
+  const mario = blogs.filter(blog => blog.author === 'mario')
+  console.log(mario)
   return (
     <>
-      <BlogList blogs={blog} title={title} />
+      <BlogList blogs={blogs} title={title} />
+      <BlogList blogs={mario} title="Mario's Blogs" />
     </>
   );
 };
